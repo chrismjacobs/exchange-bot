aaaac8ecb6a9a4419abb55d34c2bce9a87ab2e0e
ChrisEng0212 <cjx02121981@gmail.com>
2022-10-18 00:28:40 +0800

    meta

--- meta.py
+++ meta.py
@@ -5,9 +5,8 @@ from pybit import inverse_perpetual
 
 try:
     import config
-    keys = config.keys
-    API_KEY = keys[1]['api_key'],
-    API_SECRET = keys[1]['api_secret']
+    API_KEY = config.API_KEY
+    API_SECRET = config.API_SECRET
     AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID
     AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY
     SECRET_KEY = config.SECRET_KEY
@@ -21,20 +20,14 @@ except:
     API_SECRET= os.environ['API_SECRET']
 
 
-
 session = inverse_perpetual.HTTP(
     endpoint='https://api.bybit.com',
-    api_key=API_KEY,
+    api_key= API_KEY,
     api_secret=API_SECRET
 )
 
 print('bybit session', session)
 
-ws = inverse_perpetual.WebSocket(
-    test=False,
-    api_key=keys[1]['api_key'],
-    api_secret=keys[1]['api_secret']
-)
 
 s3_resource = boto3.resource('s3',
         aws_access_key_id=AWS_ACCESS_KEY_ID,
