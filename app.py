62a4b2b2a9d615392458021575e1ec42fc9f8147
ChrisEng0212 <cjx02121981@gmail.com>
2022-11-27 21:46:44 +0800

    functions

--- app.py
+++ app.py
@@ -3,22 +3,12 @@ import meta
 import json
 import time
 import base64
-import boto3
 import datetime
-from meta import session, s3_resource
+from meta import session, s3_resource, session_unauth_USD, session_unauth_USDT, r
 from pybit import usdt_perpetual, inverse_perpetual
+from function import *
 
-session_unauth_USD = usdt_perpetual.HTTP(
-    endpoint="https://api.bybit.com"
-)
-session_unauth_USDT = inverse_perpetual.HTTP(
-    endpoint="https://api.bybit.com"
-)
 
-print(session_unauth_USD.open_interest(
-    symbol="BTCUSD",
-    period="5min"
-))
 
 currentDate = datetime.date.today()
 month = currentDate.strftime("%B")
@@ -35,18 +25,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route('/')
 def home():
-    # print ('get MetaFile')
-    # bucket = 'rekt-journal'
-    # key = 'tradeJournal_' + month + '.json'
 
-    # # with open('static/' + key, 'r') as json_file:
-    # #     file_content = json_file
-
-    # content_object = s3_resource.Object( bucket, key )
-    # print(content_object)
-    # file_content = content_object.get()['Body'].read().decode('utf-8')
-    # # print(file_content)
     return 'trading desk'
+
+@app.route('/orderflow')
+def orderflow():
+
+
+    return render_template('orderflow.html')
+
+@app.route('/getOF', methods=['POST'])
+def getOF():
+    # pw = request.form ['pw']
+
+    # if pw != meta.PASSWORD:
+    #     return abort
+
+    deltaTotal = r.get('deltaTotal')
+    delta = r.get('delta')
+    stream = r.get('stream')
+
+
+    return jsonify({'deltaTotal' : deltaTotal, 'delta' : delta, 'stream' : stream})
 
 @app.route('/trade/<string:pw>')
 def trade(pw):
@@ -96,6 +96,8 @@ def journal(pw):
 
 
     return render_template('journaldesk.html', tradeJournal=json.dumps(jDict))
+
+
 
 
 @app.route('/getData', methods=['POST'])
@@ -291,7 +293,6 @@ def getOrder():
 
     return jsonify({'result' : result})
 
-
 @app.route('/recordTrade', methods=['POST'])
 def recordTrade():
     pw = request.form ['pw']
@@ -360,114 +361,14 @@ def addImage():
 
     return jsonify({'result' : json.dumps(imageSet)})
 
+@app.route('/runStream', methods=['POST', 'GET'])
+def runStream():
+    runStream()
 
+    return 'Stream Running'
 
-def setLeverage(first, stop, risk, fraction, leverage):
+from bot import *
 
-    if first == None or first == 0:
-        first = float(session.latest_information_for_symbol(symbol="BTCUSD")['result'][0]['last_price'])
-
-    distance = abs(first - stop)
-
-    percent_difference = (distance/first)*100  # as decimal
-
-    lev = round((risk/percent_difference)*fraction, 1)
-
-    print(first, stop, distance, percent_difference, lev)
-
-    if risk == 0:
-        lev = leverage
-
-    if lev < 1:
-        print('Leverage too low', lev)
-    else:
-        print(session.set_leverage(symbol="BTCUSD", leverage=lev))
-
-    return lev
-
-def getHiLow(minutes, side):
-
-    from datetime import datetime
-    now = datetime.now()
-    timestamp = int(datetime.timestamp(now)) - int(minutes)*60
-
-    data = session.query_kline(symbol="BTCUSD", interval="1", from_time=str(timestamp))['result']
-
-    print('GET HI LOW ', len(data))
-
-
-    hAry = []
-    lAry = []
-
-    for i in range(0, len(data)):
-
-        hAry.append(int(data[i]['high'].split('.')[0]))
-        lAry.append(int(data[i]['low'].split('.')[0]))
-
-    mHi = max(hAry)
-    mLow = min(lAry)
-
-    print(mLow)
-
-    if side == 'Buy':
-        return mLow
-    else:
-        return mHi
-
-def placeOrder(side, price, stop_loss, qty):
-
-    order = session.place_active_order(
-    symbol="BTCUSD",
-    side=side,
-    order_type='Limit',
-    price=price,
-    stop_loss = stop_loss,
-    take_profit = None,
-    qty=qty,
-    time_in_force="GoodTillCancel"
-    )
-
-
-    message = order['ret_msg']
-    data = json.dumps(order['result'])
-
-    print('ORDER', order)
-    print('MESSAGE', message)
-    print('DATA', data)
-
-    return data
-
-def shareImage(b64data, log, count, month):
-
-    S3_LOCATION = 'https://rekt-journal-lms.s3.ap-northeast-1.amazonaws.com/'
-    S3_BUCKET_NAME = 'rekt-journal'
-    print('PROCESSING IMAGE')
-    image = base64.b64decode(b64data)
-    filename = month + '/' + str(log) + '/' + str(count) +'.png'
-    imageLink = S3_LOCATION + filename
-    s3_resource.Bucket(S3_BUCKET_NAME).put_object(Key=filename, Body=image)
-    return imageLink
-
-def putJson(data, log, month):
-    print ('put MetaFile')
-
-
-    key = 'tradeJournal_' + month + '.json'
-    string = "static/" + key
-
-    with open(string, "r") as f:
-        jload = json.load(f)
-
-    jload[log] = json.load(data)
-
-    bucket = 'rekt-journal'
-    jstring = json.dumps(jload)
-    s3_resource.Bucket(bucket).put_object(
-        Key=key, Body=jstring)
-
-    print('json put in bucket location', bucket, key)
-
-    return 'ok'
 
 if __name__ == '__main__':
     app.run()
